2c9c22ee17f7745a2d19dc2d610ea1ed261d1726
GUERNICHE Tarek <tguerniche@cirb.brussels>
2018-12-22 19:46:17 +0100

    vol and details work

--- app.js
+++ app.js
@@ -5,52 +5,47 @@ var app = require('express')(),
 
 
 // Chargement de la page index.html
-var results = [];
+
 //app.disable('view cache');
 
 
 const connectionString = 'postgresql://postgres:admin@localhost:5432/vol'
-
-const pg = require('pg');
-const pool = new pg.Pool({
+const Pool = require('pg').Pool;
+const pool = new Pool({
     user: 'postgres',
-    host: '127.0.0.1',
+    host: 'localhost',
     database: 'vol',
     password: 'admin',
-    port: '5432'});
+    port: 5432,
+})
 
-results = pool.query('SELECT * from vol', (err, res) => {
-    console.log(res);
-results=res;
-pool.end();
+
+var getUsers = (req,res) => {pool.query('SELECT * from vol', (error, results) => {
+    if (error) {
+        throw error
+    }
+
+    res.render('vols.ejs', {results: results.rows});
+})
+
+}
+var getDetails = (req,res) => {
+    pool.query('SELECT * from vol_details where vol_id ='+req.params.vol_id, (err, response) => {
+    if (err) {
+        throw err
+    }
+    res.render('voldetails.ejs', {details:response.rows});
+
+})
+
+}
+
+app.get('/', getUsers);
+
+app.get('/voldetails/:vol_id', function(req,res){
+    getDetails(req,res);
+
 });
 
-app.get('/', function (req, res) {
-
-
-    res.render('vols.ejs', {results:results});
-});
-
-app.get('/voldetails/:vol_id', function(req, res) {
-
-    const pool = new pg.Pool({
-        user: 'postgres',
-        host: '127.0.0.1',
-        database: 'vol',
-        password: 'admin',
-        port: '5432'});
-
-    var id = req.url.id;
-
-
-    pool.query('SELECT * from vol_details where vol_id ='+req.params.vol_id, (err, res) => {
-
-        details=res;
-    pool.end();
-});
-
-
-    res.render('voldetails.ejs', {details:details});
-});
 
 server.listen(8080);

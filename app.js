fc6b3e9be0654ca57d384df3a4a78a5022e00d0f
GUERNICHE Tarek <tguerniche@cirb.brussels>
2018-12-24 17:20:59 +0100

    add boot

--- app.js
+++ app.js
@@ -9,25 +9,6 @@ const pool = new Pool({
 })
 var http = require('http');
 
-var getUsers =
-
-    pool.query('SELECT * from vol', (error, results) => {
-    if (error) {        throw error   }
-     results.rows;
-    })
-
-
-var getDetails = (req,res) => {
-    pool.query('SELECT * from vol_details where vol_id ='+req.params.vol_id, (err, response) => {
-    if (err) {
-        throw err
-    }
-    res.render('voldetails.ejs', {details:response.rows});
-
-})
-
-}
-
 fs = require('fs');
 
 /*var app = require('express')(),
@@ -35,19 +16,40 @@ fs = require('fs');
     io = require('socket.io').listen(server),
 */
 // Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('views/index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
+
+var app = require('express')(),
+    server = require('http').createServer(function(req, res) {
+        fs.readFile('views/index.html', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        });
+    }),
+    io = require('socket.io').listen(server)
+
+;
 
 
 var io = require('socket.io').listen(server);
+
+
 io.sockets.on('connection', function (socket) {
+
+    function intervalFunc() {
+        //console.log("fff    ")
+        pool.query('select * from vols where depart_date_reelle is not null and arrivee_date_reelle is null', (error, results) => {
+            if (error) {        throw error   }
+
+
+
+            socket.emit('vols',results.rows);
+     });
+    }
+
+    setInterval(intervalFunc, 1500);
+
 // Quand on client se connecte, on lui envoie un message
 
-       pool.query('SELECT * from vol', (error, results) => {
+       pool.query('select * from vols where depart_date_reelle is not null and arrivee_date_reelle is null', (error, results) => {
         if (error) {        throw error   }
         socket.emit('vols',results.rows);
       });
@@ -60,32 +62,16 @@ io.sockets.on('connection', function (socket) {
         });
 
     });
-});
-// On signale aux autres clients qu'il y a un nouveau venu
-  //  socket.broadcast.emit('message', 'Un autre client vient de se     connecter ! ');
-// Dès qu'on nous donne un pseudo, on le stocke en variable de    session
-   /*socket.on('vols', function() {
-        //socket.set('pseudo', pseudo);
-       console.log(' ffffffffff' );
-    });*/
-// Dès qu'on reçoit un "message" (clic sur le bouton), on le    note dans la console
-  //  socket.on('message', function (message) {
-// On récupère le pseudo de celui qui a cliqué dans les        variables de session
-   //     socket.get('pseudo', function (error, pseudo) {
-   //         console.log(pseudo + ' me parle ! Il me dit : ' +
-   //             message);
-  //      });
-  //  });
-   /*
+
+    io.socket.get('/ddd', function() {
+
+       socket.emit('inscription','/addVol');
 
 
+    });
 
-app.get('/voldetails/:vol_id', function(req,res){
-    getDetails(req,res);
 
 });
-app.get('/vols', getUsers);
-*/
 server.listen(8080);
 
 
